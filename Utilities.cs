a3d056c6f09395a752b00f5299914b7cc7472cc0
ElDuderini <44909968+ElDuderini@users.noreply.github.com>
2019-09-10 09:58:12 -0600

    Update Utilities.cs

--- Utilities.cs
+++ Utilities.cs
@@ -43,3 +43,18 @@ person.Name = "Igor the Ratcatcher";
 person.SayHello();
 
 base.SayHello();
+
+
+/* Useful stuff:
+ *
+ *  cube = transform.Find("Cube").gameObject; //find a reference by transform
+ *  bndCheck = GetComponent<BoundsCheck>(); // then get a script attached to that object
+ *  Vector3 vel = Random.onUnitSphere; //Random xyz velocity
+ *  vel.Normalize(); //make length of vector 1m
+ *   vel *= Random.Range(driftMinMax.x, driftMinMax.y, drifMinMax.z); //set the drift
+ *           transform.rotation = Quaternion.identity;
+        rotPerSecond = new Vector3(Random.Range(rotMinMax.x, rotMinMax.y),
+            Random.Range(rotMinMax.x, rotMinMax.y),
+            Random.Range(rotMinMax.x, rotMinMax.y));
+ *
+ */
